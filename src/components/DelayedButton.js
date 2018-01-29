692cba7d18056638afe10478c12e486b009b61ec
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-29 00:21:47 +0000

    automatically backed up by learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -4,9 +4,10 @@ class DelayedButton extends React.Component {
   handleClick = (event) => {
     console.log(event)
   }
+  
   render() {
     return (
-      <p>hello World</p>
+      <p>`${handleClick}`</p>
     )
   }
 }
